0b9db7ed677f5c2264d555e4fcf603383c534f64
khosbilegt.b <khosbilegt.b@unitel.mn>
2025-05-05 18:28:14 +0800

    changed baseurl for api

--- src/api/architect.ts
+++ src/api/architect.ts
@@ -9,7 +9,7 @@ import {
 export const architectAPI = createApi({
   reducerPath: "architectAPI",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8080/public",
+    baseUrl: "https://api-dev-cec.unitel.mn:8000/architect/v2/public",
   }),
   tagTypes: ["Flow", "FlowHandler", "FlowHandlerList", "VersionList"],
   endpoints: (build) => ({
